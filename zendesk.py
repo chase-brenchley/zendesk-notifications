b5abac522febaffe3b52e67993dcb445911b5fab
Chase Brenchley <chase.brenchley@gmail.com>
2018-04-26 15:12:40 -0600

    add verbose behavior and delete comments

--- zendesk.py
+++ zendesk.py
@@ -33,27 +33,19 @@ while True:
 		raise e
 	
 	if (current_state != results and not results.issubset(current_state)) and r.json()['count'] != 0:
-		print("Change in queue has been detected")
-		print(f"Previously there were {len(current_state)} tickets with {current_state} ids")
-		print(f"Now there are {len(results)} tickets with {results} ids")
+		if args.verbose:
+			print("Change in queue has been detected")
+			print(f"Previously there were {len(current_state)} tickets with {current_state} ids")
+			print(f"Now there are {len(results)} tickets with {results} ids")
 
 		new = [ticket['subject'] if ticket['id'] in results-current_state else None for ticket in r.json()['results']]
 		payload['body'] = "Queue update! There are {} tickets requiring attention!\n".format(len(results))
-
-		# for ticket in r.json()['results']:
-		# 	if ticket['id'] in results-current_state:
-		# 		message = requests.get('https://onapp.zendesk.com/api/v2/tickets/{}/comments.json'.format(ticket['id']), auth=HTTPBasicAuth(email,token)).json()['comments'][-1]['body']
-		# 		payload['body'] += 'Ticket Subject: {}\nMessage: {}\n'.format(ticket['subject'], message)
 
 		for subject in new:
 			if subject:
 				payload['body'] += 'Ticket Subject: {}\n'.format(subject)
 
 		requests.post('https://api.pushbullet.com/v2/pushes', data=json.dumps(payload), headers=header)
-		# time.sleep(50)
-
-	# else:
-		# print "Queue has not changed since last check"
 
 	current_state = results
 	time.sleep(10)
